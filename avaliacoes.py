188b2616e4b4950037ad506b012459a88ed0d631
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-02 18:59:30 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -13,7 +13,7 @@ from googleapiclient.http import MediaIoBaseUpload
 # ======== CONFIGURAÇÃO GOOGLE ==========
 st.sidebar.header("Configuração Google API")
 google_creds_file = st.sidebar.file_uploader("Upload credenciais Google (JSON)", type="json")
-sheet_url = st.sidebar.text_input("URL da Google Sheet", value="https://docs.google.com/spreadsheets/d/1eef9J3LerPGYIFzBtrP68GQbP6dQZy6umG195tGfveo/edit?gid=0#gid=0")
+sheet_url = st.sidebar.text_input("URL da Google Sheet", value="https://docs.google.com/spreadsheets/d/1eef9J3LerPGYIFzBtrP68GQbP6dQZy6umG195tGfveo/edit?gid=0#gid=0/edit")
 folder_id = st.sidebar.text_input("ID da pasta Google Drive para anexos", value="1ABCdEfgHiJklmnOPqRsTuvWxyZ")
 
 if google_creds_file:
@@ -188,6 +188,7 @@ if SHEET_OK and st.checkbox("Mostrar todos cadastros"):
     worksheet = sh.sheet1
     df = pd.DataFrame(worksheet.get_all_records())
     st.dataframe(df, use_container_width=True)
+
 
 
 
