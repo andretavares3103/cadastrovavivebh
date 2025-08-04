bdd95ce86120d5886757404bd7f810dad79256f0
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-04 20:48:32 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -8,7 +8,8 @@ import json
 # IDs fixos
 SHEET_ID = "1eef9J3LerPGYIFzBtrP68GQbP6dQZy6umG195tGfveo"
 FOLDER_ID = "1oYZA1foKNTapq74fCr2VDG9s4OUF3qzt"
-SHEET_URL = f"https://docs.google.com/spreadsheets/d/1eef9J3LerPGYIFzBtrP68GQbP6dQZy6umG195tGfveo/edit?gid=0#gid=0"
+SHEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/edit"
+
 
 # Google Auth/Sheets/Drive
 import gspread
@@ -195,6 +196,7 @@ if SHEET_OK and st.checkbox("Mostrar todos cadastros"):
     worksheet = sh.sheet1
     df = pd.DataFrame(worksheet.get_all_records())
     st.dataframe(df, use_container_width=True)
+
 
 
 
