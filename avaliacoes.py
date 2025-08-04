40c96d42b59737266e45debe409207ab295f916f
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-04 20:35:43 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -6,7 +6,7 @@ from io import BytesIO
 import json
 
 # IDs fixos
-SHEET_ID = "1eef9J3LerPGYIFzBtrP68GQbP6dQZy6umG195tGfveo"
+SHEET_ID = "1eef9J3LerPGYIFzBtrP68GQbP6dQZy6umG195tGfveo/edit?gid=0#gid=0"
 FOLDER_ID = "1oYZA1foKNTapq74fCr2VDG9s4OUF3qzt"
 SHEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}"
 
@@ -195,3 +195,4 @@ if SHEET_OK and st.checkbox("Mostrar todos cadastros"):
     worksheet = sh.sheet1
     df = pd.DataFrame(worksheet.get_all_records())
     st.dataframe(df, use_container_width=True)
+
