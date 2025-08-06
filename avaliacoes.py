805ea692e9cfd5e0c88ab898a1ebe3020248bb92
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-06 12:42:24 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -1,4 +1,5 @@
 
+
 import streamlit as st
 import pandas as pd
 import re
@@ -233,6 +234,7 @@ if SHEET_OK and st.checkbox("Mostrar todos cadastros"):
     worksheet = sh.worksheet("Página1")
     df = pd.DataFrame(worksheet.get_all_records())
     st.dataframe(df, use_container_width=True)
+
 
 
 
