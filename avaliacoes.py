b6c7a9aacec19c9286fea1c52974936ec59c0d45
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-07 13:11:49 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -96,7 +96,7 @@ if "cadastro_finalizado" not in st.session_state:
     st.markdown("""
     <div style='display:flex;align-items:center;gap:16px'>
         <img src='https://i.imgur.com/gIhC0fC.png' height='48'>
-        <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || PORTAL DE ATENDIMENTOS</span>
+        <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || Recrutamento e Treinamento de Profissional</span>
     </div>
     <p style='color:#666;font-size:1.08em;margin:8px 0 18px 0'>
         Consulte abaixo os atendimentos disponíveis!
@@ -325,6 +325,7 @@ if st.session_state["tela"] == "agendamento":
 #    worksheet = sh.worksheet("Página1")
 #    df = pd.DataFrame(worksheet.get_all_records())
 #    st.dataframe(df, use_container_width=True)
+
 
 
 
