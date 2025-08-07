0caffa6a6a7ceea2dbba12c6e53c3aa04e562d94
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-07 13:24:37 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -95,8 +95,8 @@ if "cadastro_finalizado" not in st.session_state:
 #st.title("Recrutamento e Treinamento de Profissional VAVIVÊ BH")
     st.markdown("""
     <div style='display:flex;align-items:center;gap:16px'>
-        <img src='https://i.imgur.com/gIhC0fC.png' height='48'>
-        <span style='font-size:3.0em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || Recrutamento e Treinamento de Profissional</span>
+        
+        <span style='font-size:2.4em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || Recrutamento e Treinamento de Profissional</span>
     </div>
 """, unsafe_allow_html=True)
 
@@ -322,6 +322,7 @@ if st.session_state["tela"] == "agendamento":
 #    worksheet = sh.worksheet("Página1")
 #    df = pd.DataFrame(worksheet.get_all_records())
 #    st.dataframe(df, use_container_width=True)
+
 
 
 
