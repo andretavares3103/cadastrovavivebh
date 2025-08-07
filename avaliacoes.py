bc2fc46f73b675945d5a25b07b4091afba6c94e4
andretavares3103 <andre.mtavares1@gmail.com>
2025-08-07 13:09:56 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -93,15 +93,15 @@ if "cadastro_finalizado" not in st.session_state:
     st.session_state["cadastro_finalizado"] = False
 
 st.title("Recrutamento e Treinamento de Profissional VAVIVÊ BH")
-        st.markdown("""
-        <div style='display:flex;align-items:center;gap:16px'>
-            <img src='https://i.imgur.com/gIhC0fC.png' height='48'>
-            <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || PORTAL DE ATENDIMENTOS</span>
-        </div>
-        <p style='color:#666;font-size:1.08em;margin:8px 0 18px 0'>
-            Consulte abaixo os atendimentos disponíveis!
-        </p>
-    """, unsafe_allow_html=True)
+    st.markdown("""
+    <div style='display:flex;align-items:center;gap:16px'>
+        <img src='https://i.imgur.com/gIhC0fC.png' height='48'>
+        <span style='font-size:1.7em;font-weight:700;color:#18d96b;letter-spacing:1px;'>BELO HORIZONTE || PORTAL DE ATENDIMENTOS</span>
+    </div>
+    <p style='color:#666;font-size:1.08em;margin:8px 0 18px 0'>
+        Consulte abaixo os atendimentos disponíveis!
+    </p>
+""", unsafe_allow_html=True)
 
 if st.session_state["tela"] == "inicio":
            # Adiciona logo/banner no topo do formulário
